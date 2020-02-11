75e94caadecab71692656b7f32afdce8c6bccd25
Angel <asuarezc91@gmail.com>
2020-02-11 18:20:07 +0100

    three municipalities

--- src/components/ContainerRight/index.js
+++ src/components/ContainerRight/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { MainMenu, DataDashboard, TextData, Content } from "./styles";
 import Query from "esri/tasks/support/Query";
 import QueryTask from "esri/tasks/QueryTask";
-import { PolygonSymbol3D } from "esri/symbols";
+
 
 export const ContainerRight = () => {
   const [list, setCount] = useState([
@@ -26,86 +26,102 @@ export const ContainerRight = () => {
 
   async function calculate() {
     const dataArray = [];
-    let objetData = {
-      "Superficie Polígonos": "",
-      "Superficie Activos": "",
-      "Precio medio de venta": "",
-      "Precio medio de alquiler": ""
-    }
+    let testArray = [];
 
-
-    const municipalities = ["'Riba-roja de Túria'","'Riba-roja de Túria'"]; 
+    const municipalities = ["'Riba-roja de Túria'", "'Almussafes'", "'Sagunto/Sagunt'"];
     for (let i = 0; i < municipalities.length; i++) {
-    
-  
-    var wildCard = [
-      "suelo_ind_spatial_50km/FeatureServer/0",
-      "actives/FeatureServer/0"];
-    for (let i = 0; i < wildCard.length; i++) {
-      const urlQuery =
-        "https://services8.arcgis.com/o9xiVBMM7LVPq4Xx/arcgis/rest/services/" + wildCard[i];
-      const queryTask = new QueryTask({
-        url: urlQuery
-      });
+      //Se van a crear tantos objetos como municipios haya
+      let objetData = {
+        "Superficie Polígonos": "",
+        "Superficie Activos": "",
+        "Precio medio de venta": "",
+        "Precio medio de alquiler": ""
+      }
       const query = new Query();
-      query.returnGeometry = true;
-      query.where = "NAMEUNIT = 'Riba-roja de Túria'";
-      if (urlQuery.includes("suelo_ind_spatial_50km")) {
-        //POLYGONS
-        let superficiesPol = [];
-        query.outFields = ["superficie"]
-        await queryTask.execute(query).then(function (results) {
-          const result = results.features;
-          for (const [index] of result.entries()) {
-            let superficie = result[index].attributes.superficie;
-            superficiesPol.push(superficie.toFixed(1));
-          }
-          let sumP = 0;
-          for (let i = 0; i < superficiesPol.length; i++) {
-            const element = Number(superficiesPol[i]);
-            sumP += element;
+      query.returnGeometry = false;
+      query.where = "NAMEUNIT = " + municipalities[i];
+      var wildCard = [
+        "suelo_ind_spatial_50km/FeatureServer/0",
+        "actives/FeatureServer/0"];
+      for (let i = 0; i < wildCard.length; i++) {
+        const urlQuery =
+          "https://services8.arcgis.com/o9xiVBMM7LVPq4Xx/arcgis/rest/services/" + wildCard[i];
+        const queryTask = new QueryTask({
+          url: urlQuery
+        });
+        if (urlQuery.includes("suelo_ind_spatial_50km")) {
+          //POLYGONS
+          let superficiesPol = [];
+          query.outFields = ["superficie"]
+          await queryTask.execute(query).then(function (results) {
+            const result = results.features;
+            for (const [index] of result.entries()) {
+              let superficie = result[index].attributes.superficie;
+              superficiesPol.push(superficie.toFixed(1));
+            }
+            let sumP = 0;
+            for (let i = 0; i < superficiesPol.length; i++) {
+              const element = Number(superficiesPol[i]);
+              sumP += element;
+            }
+            console.log("la superficie de todos los polígonos es es:" + " " + sumP);
+            objetData["Superficie Polígonos"] = sumP;
+
+          });
+        }
+        else {
+          //ACTIVES
+          let superficiesAct = [];
+          let preciosMedios = [];
+          let preciosMedioAlq = [];
+          query.outFields = ["superficie,pre_med,pred_med_a"]
+          await queryTask.execute(query).then(function (results) {
+            const result = results.features;
+            for (const [index] of result.entries()) {
+              let superficie = result[index].attributes.superficie
+              let precioMedio = result[index].attributes.pre_med;
+              let precioMedioAlq = result[index].attributes.pred_med_a;
+              superficiesAct.push(superficie.toFixed(2));
+              preciosMedios.push(precioMedio);
+              preciosMedioAlq.push(precioMedioAlq);
+            }
+            let sumA = 0;
+            for (let i = 0; i < superficiesAct.length; i++) {
+              const element = Number(superficiesAct[i]);
+              sumA += element;
+            }
+            console.log("la superficie de todos los activos es:" + " " + sumA);
+            console.log("el precio medio de venta es:" + " " + preciosMedios[0]);
+            console.log("el precio medio de alquier es:" + " " + preciosMedioAlq[0]);
+            objetData["Superficie Activos"] = sumA;
+            objetData["Precio medio de venta"] = preciosMedios[0];
+            objetData["Precio medio de alquiler"] = preciosMedioAlq[0];
+            testArray.push(sumA);
+            console.log(testArray);
+
+          });
+
+          fusion(objetData);
+
+          function fusion(objetData) {
+            dataArray.push(objetData);
+            console.log(dataArray);
           }
 
-          console.log("la superficie de todos los polígonos es es:" + " " + sumP);
-          objetData["Superficie Polígonos"]=sumP;
-        });
+        }
+
+ 
+
+
+
       }
-      else {
-        //ACTIVES
-        let superficiesAct = [];
-        let preciosMedios = [];
-        let preciosMedioAlq = [];
-        query.outFields = ["superficie,pre_med,pred_med_a"]
-        await queryTask.execute(query).then(function (results) {
-          const result = results.features;
-          for (const [index] of result.entries()) {
-            let superficie = result[index].attributes.superficie
-            let precioMedio = result[index].attributes.pre_med;
-            let precioMedioAlq = result[index].attributes.pred_med_a;
-            superficiesAct.push(superficie.toFixed(2));
-            preciosMedios.push(precioMedio);
-            preciosMedioAlq.push(precioMedioAlq);
-          }
-          let sumA = 0;
-          for (let i = 0; i < superficiesAct.length; i++) {
-            const element = Number(superficiesAct[i]);
-            sumA += element;
-          }
-          console.log("la superficie de todos los activos es:" + " " + sumA);
-          console.log("el precio medio de venta es:" + " " + preciosMedios[0]);
-          console.log("el precio medio de alquier es:" + " " + preciosMedioAlq[0]);
-          objetData["Superficie Activos"]=sumA;
-          objetData["Precio medio de venta"]=preciosMedios[0];
-          objetData["Precio medio de alquiler"]=preciosMedioAlq[0];
-        });
-      }
+
+
+
     }
 
 
-    dataArray.push(objetData);
-    console.log(dataArray);
 
-  }
 
   };
   calculate();
